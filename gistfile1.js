42dc52dc78cec7d703e56e7325fdfcadc166e7d2
Alex Zhang <alexzhangh@gmail.com>
2013-01-01 00:35:27 +0800

    add bookmarklet

--- gistfile1.js
+++ gistfile1.js
@@ -4,16 +4,16 @@ var angleX = 0; //degrees
 var angleY = 0;
 var readInput = true;
 //jquery ui sliders or html5 and place to enter numerical value for angle with longer transition time in popup alerting instructions
-if(true) { //load jquery if it doesnt exist
-done = false; //orz
+if(true) { //load jquery if it doesnt exist (for now force true to get latest version
+
 var s = document.createElement('script');
-s.type='text/javascript';document.body.appendChild(s);
+s.type='text/javascript';document.head.appendChild(s); //if body re-executed
 s.src='http://code.jquery.com/jquery-latest.js';
 console.log('jquery loaded');
     s.onload = s.onreadystatechange = function () {
-            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
-                done = true;
-				$.getScript('http://code.jquery.com/ui/1.9.2/jquery-ui.js',function(a,b,c){
+            if ((!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                console.log(jQuery);
+				jQuery.getScript('http://code.jquery.com/ui/1.9.2/jquery-ui.js',function(a,b,c){
 					if(c.status==200) {
 						main();
 					}
@@ -30,13 +30,13 @@ if(c.status==200) {
 	}
 });
 }
-
+//display actual degrees in label line
 function main() {
 $("<style type='text/css'>.keys{padding:0.1em 0.6em;border:1px solid #ccc;font-size:11px;font-family:Arial,Helvetica,sans-serif;background-color:#f7f7f7;color:#333;-moz-box-shadow:0 1px 0px rgba(0, 0, 0, 0.2),0 0 0 2px #ffffff inset;-webkit-box-shadow:0 1px 0px rgba(0, 0, 0, 0.2),0 0 0 2px #ffffff inset;box-shadow:0 1px 0px rgba(0, 0, 0, 0.2),0 0 0 2px #ffffff inset;-moz-border-radius:3px;-webkit-border-radius:3px;border-radius:3px;display:inline-block;margin:0 0.1em;text-shadow:0 1px 0 #fff;line-height:1.4;white-space:nowrap;} .rotate-ui{opacity:0.3;-webkit-transition:all 0.5s;} .rotate-ui:hover{opacity:1;} .rotate-ui > div > div {margin-bottom:10px;margin-top:5px;} #slider .ui-slider-range { background: #ef2929;} #sliderX .ui-slider-range { background: #8ae234;}  #sliderY .ui-slider-range { background: #729fcf;} </style>").appendTo("head");
 $('<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />').appendTo("head");
 $("body").wrapInner("<div class='rotate-content' />")
 $(".rotate-content").css({"-webkit-transition":"all 0.2s"}); 
-$("body").append("<div class='rotate-ui' style='text-align:left;text-color:black;z-index:999999;position:fixed;left:5px;top:5px;background-color:#eee;border:1px solid grey;font-family:Tahoma,Arial; padding:5px;'><h3>rotation controls <a href='http://zhanger.us' target='_blank'>by alex</a></h3><div class='input-state'>keyboard rotation enabled</div>Press <kbd class='keys'>a</kbd> or <kbd class='keys'>d</kbd> to rotate me!<br />Press <kbd class='keys'>w</kbd> or <kbd class='keys'>s</kbd> to rotate in the X axis. <br />Press <kbd class='keys'>q</kbd> or <kbd class='keys'>e</kbd> to rotate in the Y axis. <br /> Press <kbd class='keys'>alt</kbd> to toggle keyboard input. <br /> Press <kbd class='keys'>r</kbd> to reset all rotations <br /> note that rotating in the x or y axises may be glitchy <div style='padding:10px;'>Z axis:<div id='slider' style=''></div>X axis:<div id='sliderX' style=''></div>Y axis:<div id='sliderY' style=''></div></div></div>");
+$("body").append("<div class='rotate-ui' style='font-size:13px;text-align:left;text-color:black;z-index:999999;position:fixed;left:5px;top:5px;background-color:#eee;border:1px solid grey;font-family:Tahoma,Arial; padding:5px;'><h3>rotation controls <a href='http://zhanger.us' target='_blank'>by alex</a></h3><div class='input-state'>keyboard rotation enabled</div>Press <kbd class='keys'>a</kbd> or <kbd class='keys'>d</kbd> to rotate me!<br />Press <kbd class='keys'>w</kbd> or <kbd class='keys'>s</kbd> to rotate in the X axis. <br />Press <kbd class='keys'>q</kbd> or <kbd class='keys'>e</kbd> to rotate in the Y axis. <br /> Press <kbd class='keys'>alt</kbd> to toggle keyboard input. <br /> Press <kbd class='keys'>r</kbd> to reset all rotations <br /> note that rotating in the x or y axises may be glitchy <div style='padding:10px;'>Z axis:<div id='slider' style=''></div>X axis:<div id='sliderX' style=''></div>Y axis:<div id='sliderY' style=''></div></div></div>"); 
 $( "#slider" ).slider({range: "min",
     slide: function( event, ui ) {angle = (ui.value/100) * 360;
 	$(".rotate-content").css({"-webkit-transform":"rotate("+angle+"deg) rotateX(" + angleX + "deg) rotateY(" + angleY +"deg)"}); }
@@ -82,7 +82,7 @@ angleX=0;
 else 
 pD = false;
 if(pD) {
-e.preventDefault();
+e.preventDefault(); //prevents keypress
 console.log("default prevented");
 }
 
