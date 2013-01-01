3b30905ebd9ee1056ed042ba34e9e6a9998ea03c
Alex Zhang <alexzhangh@gmail.com>
2013-01-01 12:58:26 +0800

    add label

--- gistfile1.js
+++ gistfile1.js
@@ -1,7 +1,10 @@
 //make cross-browser
-var angle = 0; //in degrees
-var angleX = 0; //degrees
-var angleY = 0;
+/*var angle = 0; //in degrees
+var angleX = 0; 
+var angleY = 0;*/
+var angleObj = {angle:0};
+var angleXObj = {angle:0};
+var angleYObj = {angle:0};
 var readInput = true;
 //jquery ui sliders or html5 and place to enter numerical value for angle with longer transition time in popup alerting instructions
 if(true) { //load jquery if it doesnt exist (for now force true to get latest version
@@ -37,8 +40,18 @@ $('<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jque
 $('body').find('script').remove(); //remove scripts in the body to prevent re-execution in wrapInner
 $("body").wrapInner("<div class='rotate-content' />")
 $(".rotate-content").css({"-webkit-transition":"all 0.2s"}); 
-$("body").append("<div class='rotate-ui' style='font-size:13px;text-align:left;text-color:black;z-index:999999;position:fixed;left:5px;top:5px;background-color:#eee;border:1px solid grey;font-family:Tahoma,Arial; padding:5px;'><h3>rotation controls <a href='http://zhanger.us' target='_blank'>by alex</a></h3><div class='input-state'>keyboard rotation enabled</div>Press <kbd class='keys'>a</kbd> or <kbd class='keys'>d</kbd> to rotate me!<br />Press <kbd class='keys'>w</kbd> or <kbd class='keys'>s</kbd> to rotate in the X axis. <br />Press <kbd class='keys'>q</kbd> or <kbd class='keys'>e</kbd> to rotate in the Y axis. <br /> Press <kbd class='keys'>alt</kbd> to toggle keyboard input. <br /> Press <kbd class='keys'>r</kbd> to reset all rotations <br /> note that rotating in the x or y axises may be glitchy <div style='padding:10px;'>Z axis:<div id='slider' style=''></div>X axis:<div id='sliderX' style=''></div>Y axis:<div id='sliderY' style=''></div></div></div>"); 
-$( "#slider" ).slider({range: "min",
+$("body").append("<div class='rotate-ui' style='font-size:13px;text-align:left;text-color:black;z-index:999999;position:fixed;left:5px;top:5px;background-color:#eee;border:1px solid grey;font-family:Tahoma,Arial; padding:5px;'><h3>rotation controls <a href='http://zhanger.us' target='_blank'>by alex</a></h3><div class='input-state'>keyboard rotation enabled</div>Press <kbd class='keys'>a</kbd> or <kbd class='keys'>d</kbd> to rotate me!<br />Press <kbd class='keys'>w</kbd> or <kbd class='keys'>s</kbd> to rotate in the X axis. <br />Press <kbd class='keys'>q</kbd> or <kbd class='keys'>e</kbd> to rotate in the Y axis. <br /> Press <kbd class='keys'>alt</kbd> to toggle keyboard input. <br /> Press <kbd class='keys'>r</kbd> to reset all rotations <br /> note that rotating in the x or y axises may be glitchy <div style='padding:10px;'>Z axis: <span class='z-label angle-label'>0 deg</span><div id='slider' class='z-label angle-slider' style=''></div>X axis: <span class='x-label angle-label'>0 deg</span><div class='x-label angle-slider' id='sliderX' style=''></div>Y axis: <span class='y-label angle-label'>0 deg</span><div class='y-label angle-slider' id='sliderY' style=''></div></div></div>"); 
+$('.z-label').data('angle',angleObj);
+$('.y-label').data('angle',angleYObj);
+$('.x-label').data('angle',angleXObj);
+$('.angle-slider').slider({
+range:"min",
+slide: function( event, ui ) {
+$(this).data('angle').angle = (ui.value/100) * 360;
+$('.angle-label').each(function() {$(this).html(Math.round($(this).data('angle').angle) + " deg")});
+$(".rotate-content").css({"-webkit-transform":"rotate("+angleObj.angle+"deg) rotateX(" + angleXObj.angle + "deg) rotateY(" + angleYObj.angle +"deg)"}); }
+});
+/*$( "#slider" ).slider({range: "min",
     slide: function( event, ui ) {angle = (ui.value/100) * 360;
 	$(".rotate-content").css({"-webkit-transform":"rotate("+angle+"deg) rotateX(" + angleX + "deg) rotateY(" + angleY +"deg)"}); }
 });
@@ -49,14 +62,14 @@ $( "#sliderX" ).slider({range: "min",
 $( "#sliderY" ).slider({range: "min",
     slide: function( event, ui ) {angleY = (ui.value/100) * 360;
 	$(".rotate-content").css({"-webkit-transform":"rotate("+angle+"deg) rotateX(" + angleX + "deg) rotateY(" + angleY +"deg)"}); }
-});
+});*/
 $(".rotate-ui").draggable();
 /*$(document).keypress(function(e) {
 console.log(e);
 });*/
 
 $(document).keydown(function(e) {
-console.log(e);
+//console.log(e);
 if(e.which == 18) { //right alt
 readInput = !readInput;
 console.log(readInput);
@@ -65,21 +78,21 @@ $(".input-state").html("keyboard rotation " + (readInput?"enabled":"<span style=
 if(readInput) {
 pD = true;
 if(e.which == 68) // a
-angle+=2;
+angleObj.angle+=2;
 else if(e.which == 65) // d
-angle-=2;
+angleObj.angle-=2;
 else if(e.which == 87) // w
-angleX+=2;
+angleXObj.angle+=2;
 else if(e.which == 83) // s
-angleX-=2;
+angleXObj.angle-=2;
 else if(e.which == 81) // q
-angleY-=2;
+angleYObj.angle-=2;
 else if(e.which == 69) // e
-angleY+=2;
+angleYObj.angle+=2;
 else if(e.which == 82) { // r
-angle=0;
-angleY=0;
-angleX=0;
+angleObj.angle=0;
+angleYObj.angle=0;
+angleXObj.angle=0;
 }
 else 
 pD = false;
@@ -88,11 +101,12 @@ e.preventDefault(); //prevents keypress
 console.log("default prevented");
 }
 
-
-$("#slider").slider("value", angle < 0? ((1 - Math.abs(angle/360 % 1)) * 100) : ((angle/360 % 1) * 100));
+$('.angle-slider').each(function() {$(this).slider("value", $(this).data('angle').angle < 0? ((1 - Math.abs($(this).data('angle').angle/360 % 1)) * 100) : (($(this).data('angle').angle/360 % 1) * 100))});
+/*$("#slider").slider("value", angle < 0? ((1 - Math.abs(angle/360 % 1)) * 100) : ((angle/360 % 1) * 100));
 $("#sliderX").slider("value", angleX < 0? ((1 - Math.abs(angleX/360 % 1)) * 100) : ((angleX/360 % 1) * 100));
-$("#sliderY").slider("value", angleY < 0? ((1 - Math.abs(angleY/360 % 1)) * 100) : ((angleY/360 % 1) * 100));
-$(".rotate-content").css({"-webkit-transform":"rotate("+angle+"deg) rotateX(" + angleX + "deg) rotateY(" + angleY +"deg)"}); 
+$("#sliderY").slider("value", angleY < 0? ((1 - Math.abs(angleY/360 % 1)) * 100) : ((angleY/360 % 1) * 100));*/
+$('.angle-label').each(function() {$(this).html(Math.round($(this).data('angle').angle) + " deg")});
+$(".rotate-content").css({"-webkit-transform":"rotate("+angleObj.angle+"deg) rotateX(" + angleXObj.angle + "deg) rotateY(" + angleYObj.angle +"deg)"}); 
 
  }
  }
