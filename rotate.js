f0472e6b9f4823e7d999770720782079b041922a
Alex Zhang <alexzhangh@gmail.com>
2013-01-02 19:24:34 +0800

    Also including changes from master's ca5740cf4d7ecc25a92bc845abb468a28f000af3

--- rotate.js
+++ rotate.js
@@ -45,6 +45,9 @@ if (true) { //load jquery if it doesnt exist (for now force true to get latest v
     }
 }
 function main() {
+	if($.browser.opera) {
+		alert('your browser does not support transformations in the X or Y axises');
+	}
     $("<style type='text/css'>.keys{padding:0.1em 0.6em;border:1px solid #ccc;font-size:11px;font-family:Arial,Helvetica,sans-serif;background-color:#f7f7f7;color:#333;-moz-box-shadow:0 1px 0px rgba(0, 0, 0, 0.2),0 0 0 2px #ffffff inset;-webkit-box-shadow:0 1px 0px rgba(0, 0, 0, 0.2),0 0 0 2px #ffffff inset;box-shadow:0 1px 0px rgba(0, 0, 0, 0.2),0 0 0 2px #ffffff inset;-moz-border-radius:3px;-webkit-border-radius:3px;border-radius:3px;display:inline-block;margin:0 0.1em;text-shadow:0 1px 0 #fff;line-height:1.4;white-space:nowrap;} .rotate-ui{opacity:0.3;} .rotate-ui:hover{opacity:1;} .rotate-ui > div > div {margin-bottom:10px;margin-top:5px;} #slider .ui-slider-range { background: #ef2929;} #sliderX .ui-slider-range { background: #8ae234;}  #sliderY .ui-slider-range { background: #729fcf;} </style>").appendTo("head");
     $('<link rel="stylesheet" href="http://code.jquery.com/ui/1.9.2/themes/base/jquery-ui.css" />').appendTo("head");
     $('body').find('script').remove(); //remove scripts in the body to prevent re-execution in wrapInner
@@ -62,9 +65,15 @@ function main() {
             $('.angle-label').each(function () {
                 $(this).html(Math.round($(this).data('angle').angle) + " deg")
             });
+			if(!$.browser.opera) {
             $(".rotate-content").css(
                 "transform", "rotate(" + angleObj.angle + "deg) rotateX(" + angleXObj.angle + "deg) rotateY(" + angleYObj.angle + "deg)"
             );
+			} else {
+			$(".rotate-content").css(
+                "transform", "rotate(" + angleObj.angle + "deg)"
+            );
+			}
         }
     });
     $(".rotate-ui").draggable();
@@ -107,9 +116,15 @@ function main() {
                 $('.angle-label').each(function () {
                     $(this).html(Math.round($(this).data('angle').angle) + " deg")
                 });
+				if(!$.browser.opera) {
                 $(".rotate-content").css(
                     "transform","rotate(" + angleObj.angle + "deg) rotateX(" + angleXObj.angle + "deg) rotateY(" + angleYObj.angle + "deg)"
                 );
+				} else {
+				$(".rotate-content").css(
+                    "transform","rotate(" + angleObj.angle + "deg)";
+                );
+				}
 
             }
         }
